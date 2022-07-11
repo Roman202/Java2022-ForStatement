ce7ac735a917b9894030fd3a49eebe8978d067f9
Roman Hera <romahera6@gmail.com>
2022-07-11 19:58:22 +0300

    Add files via upload

---
+++ ForStatementDemo.java
@@ -0,0 +1,13 @@
+
+public class ForStatementDemo {
+	public static void main(String[] args0 ) {
+		
+		int count;
+		
+		for (count = 0; count < 5; count = count + 1)
+			System.out.println("count statement =" + count);
+		
+		System.out.println("Statement stop");
+	}
+
+}
